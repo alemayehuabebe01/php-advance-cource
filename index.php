c6289255070614ac09167ade81e0148028e96ea9
Alemayehu Abebe <alexsabebe28@gmail.com>
2025-04-02 16:34:37 +0300

    thried commit

--- index.php
+++ index.php
@@ -6,38 +6,75 @@
 
 // create cookies
 
-$expires = time() + 60 * 60 * 24 * 30;
-setcookie('test','this is sample cookie',$expires,'/');
+// $expires = time() + 60 * 60 * 24 * 30;
+// setcookie('test','this is sample cookie',$expires,'/');
 
 
-//read cookies
-if(isset($_COOKIE['test'])){
-   echo $_COOKIE['test'];
-}else{
-   echo 'cookie not set';
+// //read cookies
+// if(isset($_COOKIE['test'])){
+//    echo $_COOKIE['test'];
+// }else{
+//    echo 'cookie not set';
+// }
+
+// // session start
+// session_start(); 
+
+// //create 
+
+// $_SESSION['test'] = 'This is session variable';
+
+
+// // array_filter functions 
+
+// $array= [
+//    'a'=> 1,
+//    'b'=>2,
+//    'c'=> 3,
+//    'd'=>4,
+// ];
+
+// $filteredArray = array_filter($array, function($level){
+//    return $level != 2;
+// }, mode: 0);
+
+// print_r($filteredArray);
+
+// $array = ['alexs','john','mike','tomas'];
+// $arrays = ['deani','joh','tom','desu'];
+
+// $merged = array_merge($array,$arrays);
+
+// print_r($merged);
+
+// $newArray = [];
+// foreach($array as $value){
+//    $newArray[] = '#'.$value;
+// }
+
+// print_r($newArray);
+
+// class practice
+
+class Car{
+   public function driving(){
+      echo "I can drive ";
+   }
 }
 
-// session start
-session_start(); 
+class ElectricCar extends Car{
+   public function ecpd(){
+      echo "I am Eletric  car power contronler";
+   }
+}
 
-//create 
+class Tesla extends ElectricCar{
+   public function autoDrive(){
+      echo " I am tesla auto drive system";
+   }
+}
 
-$_SESSION['test'] = 'This is session variable';
-
-
-// array_filter functions 
-
-$array= [
-   'a'=> 1,
-   'b'=>2,
-   'c'=> 3,
-   'd'=>4,
-];
-
-$filteredArray = array_filter($array, function($level){
-   return $level == 2;
-}, mode: 0);
-
-print_r($filteredArray);
-
-
+$tesla = new Tesla();
+$tesla->driving();
+$tesla->ecpd();
+$tesla->autoDrive();
