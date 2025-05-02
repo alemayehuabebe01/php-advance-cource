5e9ef204c562a7a70a511e2cf022f6383782c6ea
Alemayehu Abebe <alexsabebe28@gmail.com>
2025-05-02 09:36:33 +0300

    curl commit

--- index.php
+++ index.php
@@ -197,11 +197,101 @@
 //regular expression
 
 //echo $_SERVER['REQUEST_URI'];
-require_once 'Router.php';
+// require_once 'Router.php';
 
-Router::handle( method: 'GET', path: '/contact',filename:'./contact.php');
-Router::handle( method: 'GET', path: '/pricing',filename:'./pricing.php');
+// Router::handle( method: 'GET', path: '/contact',filename:'./contact.php');
+// Router::handle( method: 'GET', path: '/pricing',filename:'./pricing.php');
 
-echo "index page file";
+// echo "index page file";
+
+// connect to database
+
+$pdo = new PDO(dsn:'mysql:host=localhost;dbname=phpcase',username:"root",password:'');
+//Insert data into database
+
+// $count = $pdo->exec(statement:'INSERT INTO info (name) VALUES ("Tomas")');
+
+// $pdo->beginTransaction();
+// $pdo->exec(statement:'INSERT INTO info (name) VALUES ("Php")');
+// $pdo->exec(statement:'INSERT INTO info (name) VALUES ("CSS")');
+// $pdo->exec(statement:'INSERT INTO info (name) VALUES ("CMS")');
+// $pdo->exec(statement:'INSERT INTO info (name) VALUES ("JAVA")');
+// $pdo->exec(statement:'INSERT INTO info (name) VALUES ("PhYTON")');
+// $pdo->commit();
+
+// $statement = $pdo->query("SELECT * FROM info");
+
+// $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+// //PRINT ROWS
+
+// foreach($rows as $row){
+//    echo $row['id']." - " . $row['name']. PHP_EOL;
+// }
+
+//RUN COMANDS
+ 
+
+// exec('whoami',$output,$resultCode);
+
+// echo $resultCode . PHP_EOL;
+// echo $output[0];
+
+// exec("python ./hello.py", $output, $resultCode);
+// var_dump($output);
+// echo  $resultCode;
+
+// Curl
+
+// $curl = curl_init();
+// curl_setopt_array(
+//    $curl,[
+//       CURLOPT_URL => 'https://example.com',
+//       CURLOPT_TIMEOUT=> 30,
+//       CURLOPT_CUSTOMREQUEST => 'GET',
+//       CURLOPT_HEADER => [],
+      
+//    ]
+// );
+// $responce = curl_exec($curl );
+// curl_close($curl );
+
+// echo $responce;
+
+
+$curl = curl_init();
+$requestType = 'GET';
+$url = 'https://www.imdb.com/chart/boxoffice/';
+curl_setopt_array($curl,[
+      CURLOPT_URL => $url,
+      CURLOPT_CUSTOMREQUEST => $requestType,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_RETURNTRANSFER => true,
+]);
+
+$response = curl_exec($curl);
+curl_close($curl);
+libxml_use_internal_errors(true);
+$dom = new DOMDocument();
+$dom->loadHTML($response);
+$xpath = new DOMXPath($dom);
+
+$weekendTitleNode = $xpath->query('//*[@id="boxoffice"]/h4');
+
+// Check if any nodes were found
+if ($weekendTitleNode->length > 0) {
+    $firstItem = $weekendTitleNode->item(0);
+    echo htmlspecialchars($firstItem->nodeValue);
+} else {
+    echo "No box office title found. The page structure may have changed.";
+    
+    // Debugging: Output the HTML to see current structure
+    // file_put_contents('debug.html', $response);
+}
+
+$firtItem = $weekendTitleNode->item(0);
+
+echo $firtItem->nodeValue;
+
 
 
